f59c262f5d17cfffbee0489a362d8c1d10bf28fe
Dennis Burger <dennis@dutchwebworks.nl>
2021-07-25 12:59:45 +0200

    Updated Vue example

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue Parcel SPA
+
+*By Dennis Burger, july 2021*
+
+Learning about Parcel bundler using `*.vue` -files in an SPA web site.
--- src/vue/app.vue
+++ src/vue/app.vue
@@ -6,26 +6,13 @@
 
 <script>
 export default {
-    
+    name: "app"
 }
 </script>
 
 <style lang="scss">
-body {
-    font-family: sans-serif;
-    color: white;
-    background-color: gold;
-}
-
-a {
-    color: white;
-}
-
-header,
 main {
     margin: 0 auto;
     max-width: 600px;
 }
-
-
 </style>
