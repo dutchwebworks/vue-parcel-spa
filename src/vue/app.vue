7a88ca926f2e54efcdcd312deeefb77a3bf355d5
Dennis Burger <dennis@dutchwebworks.nl>
2021-07-25 12:47:59 +0200

    Initial commit

---
+++ src/vue/app.vue
@@ -0,0 +1,31 @@
+<template>
+    <main>
+        <h1>Vue Parcel SPA</h1>
+    </main>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+body {
+    font-family: sans-serif;
+    color: white;
+    background-color: gold;
+}
+
+a {
+    color: white;
+}
+
+header,
+main {
+    margin: 0 auto;
+    max-width: 600px;
+}
+
+
+</style>
